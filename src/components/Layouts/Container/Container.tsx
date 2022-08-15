6e57f3f5192b9f0e2a80227054cfd609c9fb5dfa
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-08-15 11:28:30 +0300

    fix: error syntax

--- src/components/Layouts/Container/Container.tsx
+++ src/components/Layouts/Container/Container.tsx
@@ -1,6 +1,6 @@
 import {memo, PropsWithChildren} from "react";
 import style from "./Container.module.scss";
 
-export const Container = memo({children}:PropsWithChildren) => {
+export const Container = memo(({children}:PropsWithChildren) => {
     return (<div className={style.container}>{children}</div>)
 })
